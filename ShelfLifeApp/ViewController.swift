af75d318a793b9ff54af16aa7a7b1e0c3eda2525
Aluno Mack <alunomack@10-44-48-66.0200.mack.br.iosda.org>
2019-10-07 09:02:29 -0300

    Iniciando o Projeto

---
+++ ShelfLifeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShelfLifeApp
+//
+//  Created by Aluno Mack on 07/10/19.
+//  Copyright © 2019 TheLastFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
