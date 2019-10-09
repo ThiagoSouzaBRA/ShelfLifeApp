bc408189ea1a144f76126c01bd2351cce2ff53ae
nicolefrancanigodinho@hotmail.com <nicolefrancanigodinho@hotmail.com>
2019-10-09 11:20:01 -0300

    Implementada a table view da primeira tela e tela de adicionar

---
+++ ShelfLifeApp/DiasTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DiasTableViewCell.swift
+//  ShelfLifeApp
+//
+//  Created by Aluno Mack on 09/10/19.
+//  Copyright © 2019 TheLastFoundation. All rights reserved.
+//
+
+import UIKit
+
+class DiasTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellLabel: UILabel!
+    
+   // @IBOutlet weak var diasOutlet: UILabel!
+    
+    
+    
+    
+}
---
+++ ShelfLifeApp/PrimeiraTela.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  ShelfLifeApp
+//
+//  Created by Aluno Mack on 09/10/19.
+//  Copyright © 2019 TheLastFoundation. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DiasSemana{
+    
+    var title: String
+    var dias: Int
+    
+    init(title: String, dias: Int) {
+        self.title = title
+        self.dias = dias
+    }
+}
