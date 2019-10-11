0d5c5458f919d7a72ec8ae2105f4a0466d76ee9f
nicolefrancanigodinho@hotmail.com <nicolefrancanigodinho@hotmail.com>
2019-10-11 08:36:39 -0300

    salvando

--- ShelfLifeApp/DiasTableViewCell.swift
+++ ShelfLifeApp/DiasTableViewCell.swift
@@ -13,15 +13,24 @@ class DiasTableViewCell: UITableViewCell {
     @IBOutlet weak var cellLabel: UILabel!
     @IBOutlet weak var myView: UIView!
     
+    let cornerRadius : CGFloat = 25.0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // opacity of the shadow
- 
+        myView.layer.shadowColor = UIColor.black.cgColor
+        myView.layer.shadowOpacity = 1
+        myView.layer.shadowOffset = .zero
+        myView.layer.shadowRadius = 10
+        
+        myView.layer.shadowPath = UIBezierPath(rect: myView.bounds).cgPath
+        myView.layer.shouldRasterize = true
+        
         // coner
        myView.layer.cornerRadius = 25.0
         myView.clipsToBounds = true
-
+        
     }
    // @IBOutlet weak var diasOutlet: UILabel!
     
